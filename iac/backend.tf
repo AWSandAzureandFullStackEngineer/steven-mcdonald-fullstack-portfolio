5bb580e9ab9ebebbc977672f2ed530301c99ad32
AWSandAzureandFullStackEngineer <awsandazurecloudengineer@gmail.com>
2024-05-27 11:26:19 -0500

    upadated backend.tf and terrafrom.tfvars files.

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,9 +1,8 @@
-# store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = 
-    key            = 
-    region         = 
-    dynamodb_table = 
+    bucket         = "engineer-social-network-production-env-file-bucket"
+    key            = "steven-porflio/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-state-lock"
   }
 }
